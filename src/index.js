b679f56f1a175298e060ac1e05ce1f0246b57b31
Djonathan <djonathanmachadoo@gmail.com>
2024-05-02 14:59:07 -0300

    att

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ let message = [];
 let proxMsg = 1;
 
 app.post('/', (request, response)=>{
-    response.status(200).send(JSON.stringify({Mensagem: 'Bem vindo à aplicação!'}));
+    response.status(200).json({Mensagem: 'Bem vindo à aplicação!'});
 });
 
 app.post('/signup', async(request, response)=>{
@@ -24,26 +24,26 @@ app.post('/signup', async(request, response)=>{
     if(!nome){
         response
         .status(404)
-        .send(JSON.stringify({Mensagem: 'Por favor, verifique se passou o nome.'}))
+        .json({Mensagem: 'Por favor, verifique se passou o nome.'})
         return;
     }
     if(!email){
         response
         .status(400)
-        .send(JSON.stringify({ Mensagem: 'Por favor, verifique se passou o email.'}))
+        .json({ Mensagem: 'Por favor, verifique se passou o email.'})
         return;
     }
     if(!senha){
         response
         .status(400)
-        .send(JSON.stringify({ Mensagem: 'Por favor, verifique se passou a senha.'}))
+        .json({ Mensagem: 'Por favor, verifique se passou a senha.'})
         return;
     }
-
+    
     const verificarEmail = users.find((user)=> user.email === email)
 
     if(verificarEmail){
-        response.status(400).send(JSON.stringify({ Mensagem: 'Email já cadastrado, insira outro.'}))
+        response.status(400).json({ Mensagem: 'Email já cadastrado, insira outro.'})
         return;
     }
 
@@ -59,7 +59,7 @@ app.post('/signup', async(request, response)=>{
     users.push(novoUsuario);
     proxUser++;
 
-    response.status(201).send(JSON.stringify({ Mensagem: `Seja bem vindo ${nome}! Pessoa usuária registrada com sucesso!`}))
+    response.status(201).json({ Mensagem: `Seja bem vindo, ${nome}! Pessoa usuária registrada com sucesso!`})
 })
 
 app.get('/login', async(request, response)=> {
@@ -69,29 +69,29 @@ app.get('/login', async(request, response)=> {
     const senha = data.senha;
 
     if(!email){
-        response.status(404).send(JSON.stringify({Mensagem: 'Insira um e-mail válido.'}))
+        response.status(404).json({Mensagem: 'Insira um e-mail válido.'})
         return;
     }
     if(!senha){
-        response.status(404).send(JSON.stringify({Mensagem: 'Insira uma senha válida'}))
+        response.status(404).json({Mensagem: 'Insira uma senha válida'})
         return;
     }
 
     const user = users.find((usuarios) => usuarios.email === email);
 
     if(!user){
-        response.status(404).send(JSON.stringify({Mensagem: 'Email não encontrado no sistema, verifique ou crie uma conta.'}))
+        response.status(404).json({Mensagem: 'Email não encontrado no sistema, verifique ou crie uma conta.'})
         return;
     }
 
     const senhas = await bcrypt.compare(senha, user.senha);
 
     if(!senhas){
-        response.status(404).send(JSON.stringify({Mensagem: 'Dados inválidos, digite novamente.'}))
+        response.status(404).json({Mensagem: 'Dados inválidos, digite novamente.'})
         return;
     }
 
-    response.status(200).send(JSON.stringify({Mensagem: `Seja bem vindo ${user.nome}! Pessoa usuária logada com sucesso!`}));
+    response.status(200).json({Mensagem: `Seja bem vindo, ${user.nome}! Pessoa usuária logada com sucesso!`});
 });
 
 app.post('/message', (request, response)=>{
@@ -104,16 +104,16 @@ app.post('/message', (request, response)=>{
     const verificador = users.find(usuario => usuario.email === email);
 
     if(!verificador){
-        response.status(400).send(JSON.stringify({Mensagem: 'Email não encontrado, verifique ou crie uma conta.'}));
+        response.status(400).json({Mensagem: 'Email não encontrado, verifique ou crie uma conta.'});
         return;
     }
 
     if(!title){
-        response.status(400).send(JSON.stringify({Mensagem: 'Digite um título válido.'}))
+        response.status(400).json({Mensagem: 'Digite um título válido.'})
         return;
     }
     if(!description){
-        response.status(400).send(JSON.stringify({Mensagem: 'Digite uma descrição válida.'}))
+        response.status(400).json({Mensagem: 'Digite uma descrição válida.'})
         return;
     }
 
@@ -127,7 +127,7 @@ app.post('/message', (request, response)=>{
     message.push(newMessage);
     proxMsg++;
 
-    response.status(201).send(JSON.stringify({Mensagem: `Mensagem criada com sucesso! Título: ${title}`}));
+    response.status(201).json({Mensagem: `Mensagem criada com sucesso! Título: ${title}`});
 });
 
 app.get('/message/:email', (request, response)=>{
@@ -136,14 +136,14 @@ app.get('/message/:email', (request, response)=>{
     const verificador = users.find(usuario => usuario.email === email);
 
     if(!verificador){
-        response.status(404).send(JSON.stringify({Mensagem: 'Email não encontrado, verifique ou crie uma conta.'}));
+        response.status(404).json({Mensagem: 'Email não encontrado, verifique ou crie uma conta.'});
         return;
     }
 
     const verificar = message.filter(mensagem => mensagem.email === email);
 
     if(!verificar){
-        response.status(400).send(JSON.stringify({Mensagem: 'Não existe mensagem cadastrada.'}));
+        response.status(400).json({Mensagem: 'Não existe mensagem cadastrada.'});
         return;
     }
 
@@ -160,22 +160,22 @@ app.put('/message/:id', (request, response)=>{
     const description = data.description;
 
     if(!id){
-        response.status(400).send(JSON.stringify({Mensagem: 'Por favor, informe um ID válido da mensagem.'}));
+        response.status(400).json({Mensagem: 'Por favor, informe um ID válido da mensagem.'});
         return;
     }
 
     const idVerificado = message.findIndex(mensagem => mensagem.id === id)
 
     if(idVerificado === -1){
-        response.status(400).send(JSON.stringify({Mensagem: 'ID não encontrado.'}))
+        response.status(400).json({Mensagem: 'ID não encontrado.'})
         return;
     }
     if(!title){
-        response.status(400).send(JSON.stringify({Mensagem: 'Informe um título válido.'}));
+        response.status(400).json({Mensagem: 'Informe um título válido.'});
         return;
     }
     if(!description){
-        response.status(400).send(JSON.stringify({Mensagem: 'Informe uma descrição válida.'}));
+        response.status(400).json({Mensagem: 'Informe uma descrição válida.'});
         return;
     }    
     
